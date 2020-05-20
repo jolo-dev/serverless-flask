a5638e717b1f0e92ad5ad760a5dc6b821bcf1c40
John Long <johnyscrazy@gmail.com>
2020-05-20 14:38:57 +0200

    Make it Pyberlin Ready

--- app.py
+++ app.py
@@ -8,15 +8,5 @@ def main():
     return render_template("index.html")
 
 
-@app.route("/cats")
-def cats():
-    return render_template("cats.html")
-
-
-@app.route("/dogs/<id>")
-def dog(id):
-    return "Dog"
-
-
 if __name__ == "__main__":
     app.run()
